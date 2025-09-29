fe859a18b5cc3b25eb47466fe5621f9d0cf6bbb1
JaydenGamerR <leibyjay000@bdusd.org>
2025-09-28 19:50:30 -0500

    Added better RobotHardware Initializer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.java
@@ -1,9 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.utils.LoggingUtils.FTCDashboardPackets;
+import org.firstinspires.ftc.teamcode.utils.RobotHardwareInitializer;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.HashMap;
 
 @TeleOp(name = "Driver Op Mode (SAFE)", group = "Driver Op Mode")
 public class BasicOpMode extends LinearOpMode {
@@ -13,29 +23,49 @@ public class BasicOpMode extends LinearOpMode {
     private Robot robot;
 
     private Gamepad driverController;
+
+    private DcMotor fl_drv;
+    private DcMotor fr_drv;
+    private DcMotor bl_drv;
+    private DcMotor br_drv;
+
+
+    private final FTCDashboardPackets dbp = new FTCDashboardPackets("BasicOpMode");
+
     //private Gamepad armController; This is not needed because we are not using the 2nd controller at this time
 
     //private boolean flywheelControl = false;
 
     @Override
     public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        OpenCvWebcam camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        FtcDashboard.getInstance().startCameraStream(camera, 0);
         driverController = gamepad1;
         //armController = gamepad2;
         robot = new Robot();
         robot.initialize(hardwareMap);
 
+        // Todo: Get rid of ROBOT class and implement robot hardware initializer
+
+        fl_drv = RobotHardwareInitializer.MotorComponent.LEFT_FRONT.get(hardwareMap);
+        fr_drv = RobotHardwareInitializer.MotorComponent.RIGHT_FRONT.get(hardwareMap);
+        bl_drv = RobotHardwareInitializer.MotorComponent.LEFT_BACK.get(hardwareMap);
+        br_drv = RobotHardwareInitializer.MotorComponent.RIGHT_BACK.get(hardwareMap);
+
         // Wait for the robot to start (driver presses PLAY).
-        telemetry.addData("Status", "Initialized.");
-        telemetry.addData("Controls", "Use the following controls:");
-        telemetry.addData("Strafe Left", "Left Trigger");
-        telemetry.addData("Strafe Right", "Right Trigger");
-        telemetry.addData("Forward/Back", "LEFT Stick up/down");
-        telemetry.addData("Rotate", "RIGHT Stick left/right");
-        telemetry.addData("Flywheel On", "MUST Hold ALL Left Bumper + Right Bumper + X");
-        telemetry.addData("Flywheel Off", "Release any Left Bumper / Right Bumper / X");
-        telemetry.addData("Ethan Servo Control", "D-pad Up: Forward, D-pad Down: Reverse");
-        telemetry.addData("Good luck!", "DON'T CRASH THE ROBOT PLS :)");
-        telemetry.update();
+        dbp.createNewTelePacket();
+        dbp.info("Status: Initialized");
+        dbp.info("Controls Use the following controls:");
+        dbp.info("Strafe Left Left Trigger");
+        dbp.info("Strafe Right Right Trigger");
+        dbp.info("Forward/Back LEFT Stick up/down");
+        dbp.info("Rotate RIGHT Stick left/right");
+        dbp.info("Flywheel On MUST Hold ALL Left Bumper + Right Bumper + X");
+        dbp.info("Flywheel Off Release any Left Bumper / Right Bumper / X");
+        dbp.info("Ethan Servo Control D-pad Up: Forward, D-pad Down: Reverse");
+        dbp.info("Good luck! DON'T CRASH THE ROBOT PLS :)");
+        dbp.send(false);
 
         waitForStart();
         runtime.reset();
@@ -45,7 +75,7 @@ public class BasicOpMode extends LinearOpMode {
             updateDrive();
             updateFlywheel();
             updateEthanServo();  // Update Ethan servo control based on D-pad input
-            telemetry.update();
+            dbp.send(false);
         }
     }
 
